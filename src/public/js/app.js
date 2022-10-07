05dd5512969396664a669a3b4f7d427547b9cf39
Daehwa Seo <sdhwa92@gmail.com>
2022-10-07 15:44:54 +1000

    make a websocket connection between server and client

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connection to the Server: ✅");
+});
+
+socket.addEventListener("close", () => {
+  console.log("Connection to the Server: ❌");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New message:", message.data);
+});
+
+setTimeout(() => {
+  socket.send("Hello from the browser!");
+}, 10000);
